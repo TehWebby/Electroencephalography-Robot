2cbcfbcbd51fdbeb89e09487a4b8205c61209690
TehWebby <shaunwebbit@gmail.com>
2016-01-09 23:38:36 +0000

    Can choose which cognitive actions to train

--- EEG_Gateway/EEG_Gateway/CognitiveTraining.cs
+++ EEG_Gateway/EEG_Gateway/CognitiveTraining.cs
@@ -16,24 +16,29 @@ namespace EEG_Gateway
 
         bool write = false;
         int numOfActions = 0;
+        int totalActions = 0;
         Profile userProfile = new Profile();
+        string profileName;
+        bool[] action = new bool[4]; //used to determine how many actions to activate
+        bool[] notComplete = new bool[4];
 
+        //Total possible Cognitive actions using EPOC SDK
         public static EdkDll.EE_CognitivAction_t[] cognitivActionList = {
-                                                                     EdkDll.EE_CognitivAction_t.COG_NEUTRAL,
-                                                                     EdkDll.EE_CognitivAction_t.COG_PUSH,
-                                                                     EdkDll.EE_CognitivAction_t.COG_PULL,
-                                                                     EdkDll.EE_CognitivAction_t.COG_LIFT,
-                                                                     EdkDll.EE_CognitivAction_t.COG_DROP,
-                                                                     EdkDll.EE_CognitivAction_t.COG_LEFT,
-                                                                     EdkDll.EE_CognitivAction_t.COG_RIGHT,
-                                                                     EdkDll.EE_CognitivAction_t.COG_ROTATE_LEFT,
-                                                                     EdkDll.EE_CognitivAction_t.COG_ROTATE_RIGHT,
-                                                                     EdkDll.EE_CognitivAction_t.COG_ROTATE_CLOCKWISE,
-                                                                     EdkDll.EE_CognitivAction_t.COG_ROTATE_COUNTER_CLOCKWISE,
-                                                                     EdkDll.EE_CognitivAction_t.COG_ROTATE_FORWARDS,
-                                                                     EdkDll.EE_CognitivAction_t.COG_ROTATE_REVERSE,
-                                                                     EdkDll.EE_CognitivAction_t.COG_DISAPPEAR
-                                                                 };
+            EdkDll.EE_CognitivAction_t.COG_NEUTRAL,
+            EdkDll.EE_CognitivAction_t.COG_PUSH,
+            EdkDll.EE_CognitivAction_t.COG_PULL,
+            EdkDll.EE_CognitivAction_t.COG_LIFT,
+            EdkDll.EE_CognitivAction_t.COG_DROP,
+            EdkDll.EE_CognitivAction_t.COG_LEFT,
+            EdkDll.EE_CognitivAction_t.COG_RIGHT,
+            EdkDll.EE_CognitivAction_t.COG_ROTATE_LEFT,
+            EdkDll.EE_CognitivAction_t.COG_ROTATE_RIGHT,
+            EdkDll.EE_CognitivAction_t.COG_ROTATE_CLOCKWISE,
+            EdkDll.EE_CognitivAction_t.COG_ROTATE_COUNTER_CLOCKWISE,
+            EdkDll.EE_CognitivAction_t.COG_ROTATE_FORWARDS,
+            EdkDll.EE_CognitivAction_t.COG_ROTATE_REVERSE,
+            EdkDll.EE_CognitivAction_t.COG_DISAPPEAR
+        };
         private TextBox profileNameTxt;
         private Label newProfileLbl;
         private Label trainActionsLbl;
@@ -42,35 +47,19 @@ namespace EEG_Gateway
         private CheckBox trainBox3;
         private CheckBox trainBox2;
         private CheckBox trainBox1;
-        string profileName;
+        private Timer trainingTimer;
+        private System.ComponentModel.IContainer components;
         
 
         public void Initialize()
         {
             InitializeComponent();
-
             Show();
-            
-
-            /*Console.Write("Enter Profile Name: ");
-            profileName = Console.ReadLine();
-            profileName = "_" + profileName + ".emu";
-            Console.WriteLine(profileName);
-            /*Console.Write("Enter Profile Name: ");
-            
-
-
-            EmoEngine.Instance.EmoStateUpdated += new EmoEngine.EmoStateUpdatedEventHandler(Instance_EmoStateUpdated);
-            EmoEngine.Instance.CognitivEmoStateUpdated += new EmoEngine.CognitivEmoStateUpdatedEventHandler(Instance_CognitivEmoStateUpdated);
-            EmoEngine.Instance.CognitivTrainingStarted += new EmoEngine.CognitivTrainingStartedEventEventHandler(Instance_CognitivTrainingStarted);
-            EmoEngine.Instance.CognitivTrainingSucceeded += new EmoEngine.CognitivTrainingSucceededEventHandler(Instance_CognitivTrainingSucceeded);
-            EmoEngine.Instance.CognitivTrainingCompleted += new EmoEngine.CognitivTrainingCompletedEventHandler(Instance_CognitivTrainingCompleted);
-
-            EmoEngine.Instance.Connect();*/
         }
 
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CognitiveTraining));
             this.profileNameTxt = new System.Windows.Forms.TextBox();
             this.newProfileLbl = new System.Windows.Forms.Label();
@@ -80,6 +69,7 @@ namespace EEG_Gateway
             this.trainBox3 = new System.Windows.Forms.CheckBox();
             this.trainBox2 = new System.Windows.Forms.CheckBox();
             this.trainBox1 = new System.Windows.Forms.CheckBox();
+            this.trainingTimer = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
             // profileNameTxt
@@ -157,6 +147,10 @@ namespace EEG_Gateway
             this.trainBox1.Text = "UP";
             this.trainBox1.UseVisualStyleBackColor = true;
             // 
+            // trainingTimer
+            // 
+            this.trainingTimer.Tick += new System.EventHandler(this.trainingTimer_Tick);
+            // 
             // CognitiveTraining
             // 
             this.ClientSize = new System.Drawing.Size(414, 403);
@@ -180,9 +174,18 @@ namespace EEG_Gateway
         private void startTrainBtn_Click(object sender, EventArgs e)
         {
             profileName = profileNameTxt.Text;
+            profileName = "_" + profileName + ".emu";
 
-            bool[] action = new bool[4];
+            for (int i = 0; i < notComplete.Length; i++)
+                notComplete[i] = true;
+            
             action = checkBoxTrainingCalc(action);
+
+            for (int i = 0; i < action.Length; i++)
+            {
+                if (action[i])
+                    totalActions++;
+            }
             /*
             action[0] == UP
             action[1] == DOWN
@@ -191,6 +194,15 @@ namespace EEG_Gateway
             */
             //then use the action bool array to only allow certain cognitive actions to be trained
             MessageBox.Show(profileName);
+
+            trainingTimer.Enabled = true;
+            EmoEngine.Instance.EmoStateUpdated += new EmoEngine.EmoStateUpdatedEventHandler(Instance_EmoStateUpdated);
+            EmoEngine.Instance.CognitivEmoStateUpdated += new EmoEngine.CognitivEmoStateUpdatedEventHandler(Instance_CognitivEmoStateUpdated);
+            EmoEngine.Instance.CognitivTrainingStarted += new EmoEngine.CognitivTrainingStartedEventEventHandler(Instance_CognitivTrainingStarted);
+            EmoEngine.Instance.CognitivTrainingSucceeded += new EmoEngine.CognitivTrainingSucceededEventHandler(Instance_CognitivTrainingSucceeded);
+            EmoEngine.Instance.CognitivTrainingCompleted += new EmoEngine.CognitivTrainingCompletedEventHandler(Instance_CognitivTrainingCompleted);
+
+            EmoEngine.Instance.Connect();
         }
 
         private bool[] checkBoxTrainingCalc(bool[] action)
@@ -205,7 +217,166 @@ namespace EEG_Gateway
             }
             return action;
         }
-        
 
+
+        void StartCognitivTraining(EdkDll.EE_CognitivAction_t cognitivAction)
+        {
+            if (cognitivAction == EdkDll.EE_CognitivAction_t.COG_NEUTRAL)
+            {
+                Console.WriteLine("Neutral Training");
+                //    EmoEngine.Instance.CognitivSetActiveActions((uint)userId, 0x0000);
+            }
+            else if ((cognitivAction == EdkDll.EE_CognitivAction_t.COG_PUSH) && (action[0]))
+            {
+                Console.WriteLine("Push Training");
+                //    EmoEngine.Instance.CognitivSetActiveActions((uint)userId, 0x0002);
+            }
+            else if ((cognitivAction == EdkDll.EE_CognitivAction_t.COG_PULL) && (action[1]))
+            {
+                Console.WriteLine("Pull Training");
+                //    EmoEngine.Instance.CognitivSetActiveActions((uint)userId, 0x0004);
+            }
+            else if ((cognitivAction == EdkDll.EE_CognitivAction_t.COG_LEFT) && (action[2]))
+            {
+                Console.WriteLine("Left Training");
+                //EmoEngine.Instance.CognitivSetActiveActions((uint)userId, 0x0010);
+            }
+            else if ((cognitivAction == EdkDll.EE_CognitivAction_t.COG_RIGHT) && (action[3]))
+            {
+                Console.WriteLine("Right Training");
+                //    EmoEngine.Instance.CognitivSetActiveActions((uint)userId, 0x0012);
+            }
+
+            EmoEngine.Instance.CognitivSetTrainingAction(userId, cognitivAction);
+            EmoEngine.Instance.CognitivSetTrainingControl(userId, EdkDll.EE_CognitivTrainingControl_t.COG_START);
+        }
+
+        void SetActiveActions()
+        {
+            uint cognitivActions = 0x0000;
+            for (int i = 1; i < cognitivActionList.Length; i++)
+            {
+                cognitivActions = cognitivActions | (uint)cognitivActionList[i];
+            }
+            //EmoEngine.Instance.CognitivSetActiveActions(userId, cognitivActions);
+        }
+
+        void Instance_CognitivTrainingCompleted(object sender, EmoEngineEventArgs e)
+        {
+            Console.WriteLine("Cognitiv Training completed");
+            if ((notComplete[0]) && (action[0]))
+                push = true;
+            else if ((notComplete[1]) && (action[1]))
+                pull = true;
+            else if ((notComplete[2]) && (action[2]))
+                left = true;
+            else if ((notComplete[3]) && (action[3]))
+                right = true;
+            //else
+                //write = false;
+            
+            if (write)
+                EmoEngine.Instance.EE_SaveUserProfile((uint)userId, "Profiles\\" + profileName);
+        }
+
+        void Instance_CognitivTrainingSucceeded(object sender, EmoEngineEventArgs e)
+        {
+            Console.WriteLine("Cognitiv Training Succeeded");
+            EmoEngine.Instance.CognitivSetTrainingControl(userId, EdkDll.EE_CognitivTrainingControl_t.COG_ACCEPT);
+        }
+
+        void Instance_CognitivTrainingStarted(object sender, EmoEngineEventArgs e)
+        {
+            Console.WriteLine("Cognitiv Training started");
+        }
+
+        void Instance_CognitivEmoStateUpdated(object sender, EmoStateUpdatedEventArgs e)
+        {
+            /*
+            if (numOfActions >= totalActions)//total of action[] that is true
+            {
+                Console.WriteLine("Cognitiv EmoState Updated...");
+                EmoState es = e.emoState;
+                EdkDll.EE_CognitivAction_t currentAction = es.CognitivGetCurrentAction();
+                if (currentAction == EdkDll.EE_CognitivAction_t.COG_NEUTRAL)
+                    Console.WriteLine("Current Action is COG_NEUTRAL");
+                if (currentAction == EdkDll.EE_CognitivAction_t.COG_PUSH)
+                    Console.WriteLine("Current Action is COG_PUSH");
+                if (currentAction == EdkDll.EE_CognitivAction_t.COG_PULL)
+                    Console.WriteLine("Current Action is COG_PULL");
+                if (currentAction == EdkDll.EE_CognitivAction_t.COG_LEFT)
+                    Console.WriteLine("Current Action is COG_LEFT");
+                if (currentAction == EdkDll.EE_CognitivAction_t.COG_RIGHT)
+                    Console.WriteLine("Current Action is COG_RIGHT");
+                float power = es.CognitivGetCurrentActionPower();
+                Console.WriteLine("Current action power {0}: " + power);
+            }
+            */
+        }
+
+        void Instance_EmoStateUpdated(object sender, EmoStateUpdatedEventArgs e)
+        {
+            //Console.WriteLine("EmoState Updated..."); 
+
+            if (neutral)
+            {
+                //7 highest sensitivity
+                EmoEngine.Instance.CognitivSetActivationLevel(0, 4);
+
+                EdkDll.EE_CognitivAction_t actions =
+                EdkDll.EE_CognitivAction_t.COG_PULL
+                | EdkDll.EE_CognitivAction_t.COG_PUSH
+                | EdkDll.EE_CognitivAction_t.COG_LEFT
+                | EdkDll.EE_CognitivAction_t.COG_RIGHT;
+
+                EmoEngine.Instance.CognitivSetActiveActions((uint)userId, (UInt32)actions);
+
+                //EmoEngine.Instance.CognitivSetActiveActions((uint)userId, 0x0000 | 0x0002 | 0x0004 | 0x0010 | 0x0012);
+                StartCognitivTraining(EdkDll.EE_CognitivAction_t.COG_NEUTRAL);
+                neutral = false;
+                numOfActions++;
+            }
+            if (push)
+            {
+                StartCognitivTraining(EdkDll.EE_CognitivAction_t.COG_PUSH);
+                push = false;
+                write = true;
+                numOfActions++;
+                notComplete[0] = false;
+            }
+            else if (pull)
+            {
+                StartCognitivTraining(EdkDll.EE_CognitivAction_t.COG_PULL);
+                pull = false;
+                write = true;
+                numOfActions++;
+                notComplete[1] = false;
+            }
+            else if (left)
+            {
+                StartCognitivTraining(EdkDll.EE_CognitivAction_t.COG_LEFT);
+                write = true;
+                left = false;
+                numOfActions++;
+                notComplete[2] = false;
+            }
+            else if (right)
+            {
+                StartCognitivTraining(EdkDll.EE_CognitivAction_t.COG_RIGHT);
+                write = true;
+                right = false;
+                numOfActions++;
+                notComplete[3] = false;
+            }
+
+
+
+            //write is the save bool. Use this in the last training step (or each)
+        }
+
+        private void trainingTimer_Tick(object sender, EventArgs e)
+        {
+            EmoEngine.Instance.ProcessEvents();
+        }
     }
 }
